d7c3cf024f95db78abcc4cb22e8938401cd61ddd
Gavin Stark <gavin@gstark.com>
2018-10-03 10:22:27 -0400

    Static HTML layout

---
+++ week-05/day-02/octodex-browser/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <aside>
+          <ul>
+            <li>Umbrellatocat</li>
+            <li>Boxertocat</li>
+            <li>Hulatocat</li>
+          </ul>
+        </aside>
+        <main>Information about this cat</main>
+      </div>
+    )
+  }
+}
+
+export default App
