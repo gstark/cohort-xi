889002a52572efd43fad0d912223eb7733f36902
Jason L Perry <jason@suncoast.io>
2018-10-09 11:45:45 -0400

    Update
    A lazy commit.

--- week-06/day-2/sound-board/src/App.js
+++ week-06/day-2/sound-board/src/App.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import SoundBoard from "./SoundBoard";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+import library from "./library.json";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <>
+          <nav>
+            <ul>
+              {Object.keys(library).map(key => (
+                <li key={key}>
+                  <NavLink to={`/${key}`}>{library[key].title}</NavLink>
+                </li>
+              ))}
+            </ul>
+          </nav>
+          <Route path="/:name" component={SoundBoard} />
+        </>
+      </Router>
     );
   }
 }
---
+++ week-06/day-2/sound-board/src/SoundButton.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+class SoundButton extends Component {
+  constructor(props) {
+    super(props);
+    this.audioRef = React.createRef();
+  }
+
+  playSound = () => {
+    const audio = this.audioRef.current;
+    audio.pause();
+    audio.currentTime = 0;
+    audio.play();
+  };
+
+  render() {
+    return (
+      <div
+        className="sound-button"
+        onClick={this.playSound}
+        style={{ backgroundImage: `url(${this.props.thumb})` }}
+      >
+        <audio preload="true" ref={this.audioRef}>
+          <source src={`/sounds/${this.props.sound}.mp3`} type="audio/mp3" />
+        </audio>
+      </div>
+    );
+  }
+}
+
+export default SoundButton;
