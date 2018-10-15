650b1a11f2efead7f6b25d3d7c01d206894746fb
ooeygooeygold <amichaelj1@gmail.com>
2018-10-15 10:20:27 -0400

    start of snowman

---
+++ week-07/snowman/src/App.jsx
@@ -0,0 +1,104 @@
+import React, { Component } from 'react'
+import snowman_0 from './snowman/step_0.png'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <img src={snowman_0} />
+        <ul>
+          <li>_</li>
+          <li>_</li>
+          <li>_</li>
+          <li>_</li>
+          <li>_</li>
+          <li>_</li>
+          <li>_</li>
+        </ul>
+        <ul>
+          <li>
+            <button>a</button>
+          </li>
+          <li>
+            <button>b</button>
+          </li>
+          <li>
+            <button>c</button>
+          </li>
+          <li>
+            <button>d</button>
+          </li>
+          <li>
+            <button>e</button>
+          </li>
+          <li>
+            <button>f</button>
+          </li>
+          <li>
+            <button>g</button>
+          </li>
+          <li>
+            <button>h</button>
+          </li>
+          <li>
+            <button>i</button>
+          </li>
+          <li>
+            <button>j</button>
+          </li>
+          <li>
+            <button>k</button>
+          </li>
+          <li>
+            <button>l</button>
+          </li>
+          <li>
+            <button>m</button>
+          </li>
+          <li>
+            <button>n</button>
+          </li>
+          <li>
+            <button>o</button>
+          </li>
+          <li>
+            <button>p</button>
+          </li>
+          <li>
+            <button>q</button>
+          </li>
+          <li>
+            <button>r</button>
+          </li>
+          <li>
+            <button>s</button>
+          </li>
+          <li>
+            <button>t</button>
+          </li>
+          <li>
+            <button>u</button>
+          </li>
+          <li>
+            <button>v</button>
+          </li>
+          <li>
+            <button>w</button>
+          </li>
+          <li>
+            <button>x</button>
+          </li>
+          <li>
+            <button>y</button>
+          </li>
+          <li>
+            <button>z</button>
+          </li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default App
