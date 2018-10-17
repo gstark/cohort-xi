cb216f6dfebf6e82ff2ba4966ee86c51caea567d
Gavin Stark <gavin@gstark.com>
2018-10-17 11:28:07 -0400

    Red bricks

--- week-07/day-03/breakout/README.md
+++ week-07/day-03/breakout/README.md
@@ -3,17 +3,21 @@
 ## Ball
 
 - Bounces of
-  - world bounds
-  - paddle
+  - X world bounds
+    - X left
+    - X right
+    - X top
+  - lose when bounces off bottom world bound
+  - X paddle
   - bricks
   - basically everything
   - may get more velocity based on where on the paddle it hits and how fast the paddle is moving
 
 ## Paddle
 
-- lateral movement
-- bounded by the screen
-- no vertical movement
+- X lateral movement
+- X bounded by the screen
+- X no vertical movement
 - might have varying speed?
 
 ## Bricks
--- week-07/day-03/breakout/public/main.js
+++ week-07/day-03/breakout/public/main.js
@@ -19,9 +19,11 @@ let config = {
 let cursors
 let paddle
 let ball
+let bricks
 
 function preload() {
   this.load.image('paddle', 'assets/paddle.png')
+  this.load.image('brick', 'assets/brick.png')
   this.load.image('ball', 'assets/ball.png')
 }
 
@@ -37,19 +39,32 @@ function update() {
 
 function paddleHit() {}
 
+function brickHit(ball, brick) {
+  brick.disableBody(true, true)
+}
+
 function create() {
+  bricks = this.physics.add.staticGroup()
+
+  for (let row = 0; row < 5; row++) {
+    for (let col = 0; col < 5; col++) {
+      let brick = bricks.create(70 + col * 150, 25 + row * 50, 'brick')
+    }
+  }
+
   paddle = this.physics.add.sprite(200, 590, 'paddle')
   paddle.body.allowGravity = false
   paddle.body.collideWorldBounds = true
   paddle.body.immovable = true
 
-  ball = this.physics.add.sprite(0, 16, 'ball').setScale(0.1)
+  ball = this.physics.add.sprite(0, 300, 'ball').setScale(0.1)
   ball.setBounce(1)
   ball.setCollideWorldBounds(true)
   ball.setVelocity(400, 400)
   ball.body.allowGravity = false
 
   this.physics.add.collider(paddle, ball, paddleHit)
+  this.physics.add.collider(bricks, ball, brickHit)
 
   cursors = this.input.keyboard.createCursorKeys()
 }
