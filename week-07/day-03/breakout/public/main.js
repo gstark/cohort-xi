1ac0876763c7982d34df9246d41bca9a9e3c4328
Gavin Stark <gavin@gstark.com>
2018-10-17 11:14:40 -0400

    Bouncing ball

--- week-07/day-03/breakout/public/main.js
+++ week-07/day-03/breakout/public/main.js
@@ -18,25 +18,38 @@ let config = {
 
 let cursors
 let paddle
+let ball
 
 function preload() {
   this.load.image('paddle', 'assets/paddle.png')
+  this.load.image('ball', 'assets/ball.png')
 }
 
 function update() {
   if (cursors.left.isDown) {
-    paddle.setVelocityX(-160)
+    paddle.setVelocityX(-320)
   }
 
   if (cursors.right.isDown) {
-    paddle.setVelocityX(160)
+    paddle.setVelocityX(320)
   }
 }
 
+function paddleHit() {}
+
 function create() {
-  paddle = this.physics.add.sprite(200, 550, 'paddle')
+  paddle = this.physics.add.sprite(200, 590, 'paddle')
   paddle.body.allowGravity = false
   paddle.body.collideWorldBounds = true
+  paddle.body.immovable = true
+
+  ball = this.physics.add.sprite(0, 16, 'ball').setScale(0.1)
+  ball.setBounce(1)
+  ball.setCollideWorldBounds(true)
+  ball.setVelocity(400, 400)
+  ball.body.allowGravity = false
+
+  this.physics.add.collider(paddle, ball, paddleHit)
 
   cursors = this.input.keyboard.createCursorKeys()
 }
