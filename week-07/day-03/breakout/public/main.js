b229e06d4cac0d84e6c24809310d3b2fd9f1ef7b
Gavin Stark <gavin@gstark.com>
2018-10-17 15:29:22 -0400

    Animated breakout ball

--- week-07/day-03/breakout/public/main.js
+++ week-07/day-03/breakout/public/main.js
@@ -19,11 +19,11 @@ let config = {
 const initialBallPosition = {
   x: 0,
   y: 300,
-  velocityX: 600,
-  velocityY: 600
+  velocityX: Phaser.Math.FloatBetween(300, 400),
+  velocityY: Phaser.Math.FloatBetween(300, 400)
 }
 
-const paddleVelocity = 600
+const paddleVelocity = 1200
 
 let cursors
 let paddle
@@ -34,8 +34,11 @@ let particles
 function preload() {
   this.load.image('paddle', 'assets/paddle.png')
   this.load.image('brick', 'assets/brick.png')
-  this.load.image('ball', 'assets/ball.png')
   this.load.image('red', 'assets/red.png')
+  this.load.spritesheet('ball', 'assets/ball-spritesheet.png', {
+    frameWidth: 224,
+    frameHeight: 224
+  })
 }
 
 function resetGame() {
@@ -60,14 +63,31 @@ function update() {
 
   if (cursors.left.isDown) {
     paddle.setVelocityX(-paddleVelocity)
-  }
-
-  if (cursors.right.isDown) {
+  } else if (cursors.right.isDown) {
     paddle.setVelocityX(paddleVelocity)
+  } else {
+    paddle.setVelocityX(0)
   }
 }
 
-function paddleHit() {}
+function paddleHit(paddle, ball) {
+  console.log('HIT')
+  var diff = 0
+
+  if (ball.x < paddle.x) {
+    //  Ball is on the left-hand side of the paddle
+    diff = paddle.x - ball.x
+    ball.body.velocity.x = -5 * diff
+  } else if (ball.x > paddle.x) {
+    //  Ball is on the right-hand side of the paddle
+    diff = ball.x - paddle.x
+    ball.body.velocity.x = 5 * diff
+  } else {
+    //  Ball is perfectly in the middle
+    //  Add a little random X to stop it bouncing straight up!
+    ball.body.velocity.x = 2 + Math.random() * 8
+  }
+}
 
 function brickHit(ball, brick) {
   let emitter = particles.createEmitter({
@@ -105,13 +125,25 @@ function create() {
   paddle.body.collideWorldBounds = true
   paddle.body.immovable = true
 
-  ball = this.physics.add
-    .sprite(initialBallPosition.x, initialBallPosition.y, 'ball')
-    .setScale(0.1)
+  ball = this.physics.add.sprite(
+    initialBallPosition.x,
+    initialBallPosition.y,
+    'ball'
+  )
   ball.setBounce(1)
+  ball.setScale(0.2)
   ball.setCollideWorldBounds(true)
   ball.setVelocity(initialBallPosition.velocityX, initialBallPosition.velocityY)
   ball.body.allowGravity = false
+
+  this.anims.create({
+    key: 'bouncing',
+    frames: this.anims.generateFrameNumbers('ball', { start: 0, end: 11 }),
+    frameRate: 5,
+    repeat: -1
+  })
+
+  ball.anims.play('bouncing', true)
 
   this.physics.add.collider(paddle, ball, paddleHit)
   this.physics.add.collider(bricks, ball, brickHit, null, this)
