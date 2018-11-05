e92dffc02b6b23090984cf47463d78023c38923b
Mark Dewey <mark.t.dewey@gmail.com>
2018-11-05 09:49:04 -0500

    added basics for react

---
+++ dotnet/week-10/coffee-shop-client/src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+    render() {
+        return (
+            <div>
+                Check out these shops
+            </div>
+        );
+    }
+}
+
+export default HomePage;
