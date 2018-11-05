32a91fe850d1514332f9b5e6ff3e0ecdc9621d10
Mark Dewey <mark.t.dewey@gmail.com>
2018-11-05 09:55:53 -0500

    Added Bulma

--- dotnet/week-10/coffee-shop-client/src/Pages/Splash.jsx
+++ dotnet/week-10/coffee-shop-client/src/Pages/Splash.jsx
@@ -4,10 +4,18 @@ import { Link } from 'react-router-dom';
 class SplashPage extends Component {
     render() {
         return (
-            <div>
-                Lets find some Coffee
-                <Link to="/home">Begin</Link>
-            </div>
+            <section class="hero is-medium is-dark is-bold">
+                <div class="hero-body">
+                    <div class="container">
+                        <h1 class="title">
+                            Lets find some coffee shops
+                        </h1>
+                        <h2 class="subtitle">
+                            <Link to="/home">Start</Link>
+                        </h2>
+                    </div>
+                </div>
+            </section>
         );
     }
 }
