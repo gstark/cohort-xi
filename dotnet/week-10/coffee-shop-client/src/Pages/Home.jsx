2eef96f65aba28561ba39b75d8de09983d82c562
Andrew Lanza <andrewlanza@Andrews-MBP-2.localdomain>
2018-11-06 11:13:16 -0500

    added search

--- dotnet/week-10/coffee-shop-client/src/Pages/Home.jsx
+++ dotnet/week-10/coffee-shop-client/src/Pages/Home.jsx
@@ -1,39 +1,58 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 class HomePage extends Component {
+  state = {
+    locations: [],
+    searchTerm: ""
+  };
 
+  componentDidMount() {
+    axios.get("https://localhost:5001/api/locations").then(json => {
+      console.log({ json });
+      this.setState({
+        locations: json.data
+      });
+    });
+  }
 
-    state = {
-        locations: []
-    }
+  handleSearchTermUpdate = e => {
+    this.setState({ searchTerm: e.target.value });
+  };
 
-    componentDidMount() {
-        axios.get('https://localhost:5001/api/locations')
-            .then(json => {
-                console.log({ json });
-                this.setState({
-                    locations: json.data
-                })
-            })
-    }
+  handleSearch = e => {
+    e.preventDefault();
+    axios
+      .get(
+        `https://localhost:5001/api/search?searchTerm=${this.state.searchTerm}`
+      )
+      .then(json => {
+        console.log({ json });
+        this.setState({
+          locations: json.data
+        });
+      });
+  };
 
-    render() {
-        return (
-            <div>
-                <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
-                    <div className="navbar-brand">
-                        <a className="navbar-item" href="/">
-                            Coffee Ship, Sail the Java Seas.
-                        </a>
+  render() {
+    return (
+      <div>
+        <nav
+          className="navbar is-dark"
+          role="navigation"
+          aria-label="main navigation"
+        >
+          <div className="navbar-brand">
+            <a className="navbar-item" href="/">
+              Coffee Ship, Sail the Java Seas.
+            </a>
+          </div>
 
-                    </div>
-
-                    {/* <div id="navbarBasicExample" className="navbar-menu">
+          {/* <div id="navbarBasicExample" className="navbar-menu">
 
 
                     </div> */}
 
-                    {/* <div className="navbar-end">
+          {/* <div className="navbar-end">
                         <div className="navbar-item">
                             <div className="buttons">
                                 <a className="button is-primary">
@@ -45,63 +64,72 @@ class HomePage extends Component {
                             </div>
                         </div>
                     </div> */}
-                </nav>
+        </nav>
 
-                <form className="search-form">
-                    <div className="field has-addons">
-                        <div className="control">
-                            <input className="input" type="text" placeholder="Find a coffee harbor" />
-                        </div>
-                        <div className="control">
-                            <a className="button is-info">
-                                Search
-                             </a>
-                        </div>
-                        <div className="control">
-                            <a className="button is-primary">
-                                Near me
-                             </a>
-                        </div>
-                    </div>
-                </form>
+        <form onSubmit={this.handleSearch} className="search-form">
+          <div className="field has-addons">
+            <div className="control">
+              <input
+                onChange={this.handleSearchTermUpdate}
+                className="input"
+                type="text"
+                placeholder="Find a coffee harbor"
+              />
+            </div>
+            <div className="control">
+              <a className="button is-info">Search</a>
+            </div>
+            <div className="control">
+              <a className="button is-primary">Near me</a>
+            </div>
+          </div>
+        </form>
 
-                <section className="coffee-shop-list">
-                    {this.state.locations.map(shop => {
-                        return <div className="card" key={shop.id}>
-                            {/* <div className="card-image">
+        <section className="coffee-shop-list">
+          {this.state.locations.map(shop => {
+            return (
+              <div className="card" key={shop.id}>
+                {/* <div className="card-image">
                         <figure className="image is-4by3">
                             <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
                         </figure>
                     </div> */}
-                            <div className="card-content">
-                                <div className="media">
-                                    <div className="media-left">
-                                        <figure className="image is-48x48">
-                                            <img src={shop.imageUrl} alt={shop.franchise.brand} />
-                                        </figure>
-                                    </div>
-                                    <div className="media-content">
-                                        <p className="title is-4">{shop.franchise.brand}</p>
-                                        <p className="subtitle is-6">{shop.address} {shop.city} {shop.state} {shop.zip}</p>
-                                    </div>
-                                </div>
+                <div className="card-content">
+                  <div className="media">
+                    <div className="media-left">
+                      <figure className="image is-48x48">
+                        <img src={shop.imageUrl} alt={shop.franchise.brand} />
+                      </figure>
+                    </div>
+                    <div className="media-content">
+                      <p className="title is-4">{shop.franchise.brand}</p>
+                      <p className="subtitle is-6">
+                        {shop.address} {shop.city} {shop.state} {shop.zip}
+                      </p>
+                    </div>
+                  </div>
 
-                                <div className="content">
-                                    <ul>
-                                        {/* <li>Distance: 4.7 miles</li> */}
-                                        <li>Rating:{shop.rating}/5</li>
-                                        {/* <li>Hours:9pm -10pm</li> */}
-                                    </ul>
-                                    <br />
-                                    <a href="/details">more details</a>
-                                </div>
-                            </div>
-                        </div>
-                    })}
-                </section>
-            </div >
-        );
-    }
+                  <div className="content">
+                    <ul>
+                      {/* <li>Distance: 4.7 miles</li> */}
+                      <li>
+                        Rating:
+                        {shop.rating}
+                        /5
+                      </li>
+                      {/* <li>Hours:9pm -10pm</li> */}
+                    </ul>
+                    <br />
+                    <a href="/details">more details</a>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </section>
+      </div>
+    );
+  }
 }
 
 export default HomePage;
