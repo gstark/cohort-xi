0e9e64aeb987e442a5a790f42e75979d8d991e3a
Andrew Lanza <andrewlanza@Andrews-MBP-2.localdomain>
2018-11-06 11:34:06 -0500

    API werks

---
+++ dotnet/week-10/CoffeeShipAPI/Controllers/SearchController.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoffeeShipAPI.Controllers
+
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class SearchController : ControllerBase
+  {
+
+
+    [HttpGet]
+
+    public ActionResult Get([FromQuery] String searchTerm)
+    {
+      var db = new CoffeeShopFinderContext();
+
+      //connect to database v
+      var results = db.Locations.Include(i => i.Franchise).Where(w => w.Franchise.Brand.ToLower().Contains(searchTerm.ToLower()));
+
+
+
+
+
+      // query for locations that have where brand/city/zip contains search term
+      return Ok(results);
+
+
+
+      // return results 
+    }
+
+  }
+
+
+
+}
+
+
