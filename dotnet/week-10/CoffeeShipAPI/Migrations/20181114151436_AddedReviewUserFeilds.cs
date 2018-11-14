b3a898babe901c838e7223742d2ebedc2ac6867e
Mark Dewey <mark.t.dewey@gmail.com>
2018-11-14 10:16:50 -0500

    added reviews for user reviews

---
+++ dotnet/week-10/CoffeeShipAPI/Migrations/20181114151436_AddedReviewUserFeilds.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CoffeeShipAPI.Migrations
+{
+    public partial class AddedReviewUserFeilds : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ReviewerName",
+                table: "Reviews",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ReviewerPhoto",
+                table: "Reviews",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Reviews",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReviewerName",
+                table: "Reviews");
+
+            migrationBuilder.DropColumn(
+                name: "ReviewerPhoto",
+                table: "Reviews");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Reviews");
+        }
+    }
+}
